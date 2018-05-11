44a5bec8e8d12ca35cf02312bfeb882ac54d3f14
ledazzler <ledazzler@users.noreply.github.com>
2018-05-11 18:46:44 +0100

    update
    added list of api keys, function to choose useable api key, output to console for debugging

--- urlvoid_url.py
+++ urlvoid_url.py
@@ -9,10 +9,29 @@ import time
 
 timestr = time.strftime("%Y-%m-%d-%H%M")
 namestr = 'URLVOID_report' + timestr +'.csv'
-apikey = input('please enter API key: ')
 
-def checkurl(url):
-  urlstring = "http://api.urlvoid.com/api1000/" + apikey + "/host/" + url
+key1 = ################
+key2 = ################
+key3 = ################
+
+apikeylist = [key1,key2,key3]
+
+def remaining_queries(key):
+  remaining_url = 'http://api.urlvoid.com/api1000/' + key  + '/stats/remained/'
+  contents = urllib.request.urlopen(remaining_url).read()
+  
+  et_xml = ET.fromstring(contents)
+  root = et_xml
+  
+  remained = [ b.text for b in root.iterfind(".//queriesRemained")]
+  remainedint = int(remained[0])
+  keyend = key[-4:]
+  #print('key ending {} has {} queries remaining today'.format(keyend,(remainedint)))
+  return(remainedint)
+
+
+def checkurl(url,key):
+  urlstring = "http://api.urlvoid.com/api1000/" + key + "/host/" + url
   contents = urllib.request.urlopen(urlstring).read()
 
   et_xml = ET.fromstring(contents)
@@ -40,14 +59,33 @@ def checkurl(url):
   except Exception as e:
     logger.error('Failed to write to csv: '+ str(e))
 
-def urlvoid():
-  urlvoid_file = input('please enter file name: ')
-  with open (urlvoid_file,'r') as f:
-    for x in f:
-      try:
-        print('submitting {}'.format(x))
-        checkurl(x)
-      except Exception as e:
-        logger.error('Failed to submit {}: '+ str(e)).format(x)
+def choosekey(keylist):
+  for key in keylist:
+    counter = remaining_queries(key)
+    keyend = key[-4:]
+    try:
+      if counter == 0:
+        print('key ending {} has used all queries for today'.format(keyend))
+      elif counter > 978:
+        print ('choosekye func selecting key ending {}'.format(keyend))
+        return(key)
+    except Exception as e:
+      logger.error('all keys have been used for today')
+      return None
+      
+def urlvoid(file):
+  with open(file,'r') as f:
+    #remove blank space
+    lines = (line.rstrip() for line in f) 
+    lines = list(line for line in lines if line)
+    for x in lines:
+      key = choosekey(apikeylist)
+      keyend = key[-4:]
+      print('urlvoid func using key ending {} to submit {}'.format(keyend,x))
+      checkurl(x,key)
+    
+#urlvoid('test.csv')
 
-urlvoid()
+#for x in apikeylist:
+#  print(remaining_queries(x))
+# Next add error check for no more useable keys
